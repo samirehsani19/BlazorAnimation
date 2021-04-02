d7b4f7a7acb8f1ee2c40ef6077d11a8cc5fba51b
samirehsani19 <samir.ehsani@learnet.se>
2021-04-02 10:09:53 +0200

    animation

---
+++ BlazorAnimation/wwwroot/custom.js
@@ -0,0 +1,34 @@
+﻿var container;
+var pagination;
+
+// getting pegination class name
+//function GetPagination(className) {
+//    pagination = className;
+//    SwipeImages();
+//}
+
+//Getting container class name
+function GetContainer(className) {
+    container = className;
+    pagination = "swiper-pagination"; // accessing by class name 
+    SwipeImages();
+}
+
+
+//swiping image
+function SwipeImages() {
+
+    var swiper = new Swiper("." + container, {
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 50,
+            shadowScale: 1,
+        },
+        pagination: {
+            el: "." + pagination,
+        },
+    });
+}
