57b665c6fb304425e724f00515ca73dc87ee0ed2
samirehsani19 <samir.ehsani@learnet.se>
2021-04-05 11:58:51 +0200

    communication between c# and js

---
+++ BlazorAnimation/wwwroot/Communication.js
@@ -0,0 +1,26 @@
+﻿// C# calling this method
+function GetResult(num1, num2) {
+    return num1 * num2;
+}
+
+
+var jsFunctions = {};
+jsFunctions.GetSum = function () { // calling C# method
+    var num1 = prompt("Enter first number");
+    var num2 = prompt("Enter second number");
+    DotNet.invokeMethodAsync("BlazorAnimation", "GetSum", parseInt(num1), parseInt(num2))
+        .then(result => {
+            console.log(result);
+            var el = document.getElementById("result");
+            el.innerHTML = 'the summation of your numbers is: ' + result;
+        }
+    );
+}
+
+
+
+ // calling c# void method, dotNetObjRef needs for calling void method
+jsFunctions.GetInstance = function (dotNetObjRef) { 
+    var name = prompt("Enter your name");
+    dotNetObjRef.invokeMethodAsync("GetName", name);
+}
